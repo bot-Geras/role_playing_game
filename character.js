f8287de177a4ca72d971e0c7a22a34640595e8a6
Brian <briandave771@gmail.com>
2022-05-09 23:58:05 +0300

    added the end game function

--- character.js
+++ character.js
@@ -1,19 +1,32 @@
-import {getDiceRollArray} from './utils/utils.js'
+import {getDiceRollArray, getDicePlaceholderHtml} from './utils/utils.js'
 
 function Character(data) {
     Object.assign(this, data);
+
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)
   
-    this.getDiceHtml = function (diceCount) {
-      return getDiceRollArray(diceCount)
-        .map(function (num) {
-          return `<div class="dice">${num}</div>`;
-        })
-        .join("");
+    this.getDiceHtml = function () {
+      this.currentDiceScore = getDiceRollArray(this.diceCount)
+      this.diceArray = this.currentDiceScore.map(function(num) {
+        return `<div class="dice">${num}</div>`
+      }).join('')
     };
+
+    this.takeDamage = function (attackScoreArray) {
+
+      const totalAttackScore = attackScoreArray.reduce(function(total, num) {
+        return total + num
+      })
+      this.health -= totalAttackScore
+      if (this.health <= 0) {
+        this.dead = true
+        this.health = 0
+        console.log(this.dead);
+      }
+    }
   
     this.getCharacterHtml = function () {
-      const { elementId, name, avatar, health, diceCount } = this;
-      let diceHtml = this.getDiceHtml(diceCount);
+      const {name, avatar, health, diceCount } = this;
   
       return `
               <div class="character-card">
@@ -21,7 +34,7 @@ function Character(data) {
                   <img class="avatar" src="${avatar}" />
                   <div class="health">health: <b> ${health} </b></div>
                   <div class="dice-container">
-                      ${diceHtml}
+                      ${this.diceArray}
                   </div>
               </div>`;
     };
