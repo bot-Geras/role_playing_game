99af29e42921b40ca3be077710a6d1085a19cf73
Brian <briandave771@gmail.com>
2022-05-10 06:08:26 +0300

    Second Commit

--- index.js
+++ index.js
@@ -1,32 +1,73 @@
 import characterData from "./data/data.js";
 import Character from "./character.js";
 
-function attack() {
-  wizard.getDiceHtml();
-  orc.getDiceHtml();
-  wizard.takeDamage(orc.currentDiceScore);
-  orc.takeDamage(wizard.currentDiceScore);
-  render();
+let monstersArray = ["orc", "demon", "goblin"];
+let isWaiting = false
 
-  if (wizard.dead || orc.dead) {
-    endGame();
+function getNewMonster() {
+  const nextMonsterData = characterData[monstersArray.shift()];
+  return nextMonsterData ? new Character(nextMonsterData) : {};
+}
+
+function attack() {
+  if(!isWaiting){
+      wizard.setDiceHtml()
+      monster.setDiceHtml()
+      wizard.takeDamage(monster.currentDiceScore)
+      monster.takeDamage(wizard.currentDiceScore)
+      render()
+      
+      if(wizard.dead){
+          endGame()
+      }
+      else if(monster.dead){
+          isWaiting = true
+          if(monstersArray.length > 0){
+              setTimeout(()=>{
+                  monster = getNewMonster()
+                  render()
+                  isWaiting = false
+              },1500)
+          }
+          else{
+              endGame()
+          }
+      }    
   }
+
 }
 
 function endGame() {
-  const endMessage = wizard.health === 0 && orc.health === 0 ? "No victors - all creatures are dead"
-      : wizard.health > 0 ? "The Wizard Wins"
-      : "The Orc is Victorious";
-  console.log(endMessage);
+  isWaiting = true
+  const endMessage =
+    wizard.health === 0 && monster.health === 0
+      ? "No victors - all creatures are dead"
+      : wizard.health > 0
+      ? "The Wizard Wins"
+      : "The Monsters are Victorious";
+  const endEmoji = wizard.health > 0 ? "🔮" : "☠️";
+
+  setTimeout(() => {
+    document.body.innerHTML = `
+  <div class="end-game">
+        <h2>Game Over</h2>
+        <h3>${endMessage}</h3>
+        <p class="end-emoji">${endEmoji}</p>
+    </div>`;
+
+  }, 1500)
+
+  
 }
 
 function render() {
   document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
-  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+  document.getElementById("monster").innerHTML = monster.getCharacterHtml();
 }
 
 document.getElementById("attack-button").addEventListener("click", attack);
 
 const wizard = new Character(characterData.hero);
-const orc = new Character(characterData.monster);
+
+let monster = getNewMonster();
 render();
